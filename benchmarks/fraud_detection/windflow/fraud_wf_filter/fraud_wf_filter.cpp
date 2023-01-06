7cc020926a96c873239a3d68cc7e6b25d388bf9c
Adriano Garcia <adriano.garcia@edu.pucrs.br>
2023-01-06 15:11:13 -0300

    Removing global namespaces

--- benchmarks/fraud_detection/windflow/fraud_wf_filter/fraud_wf_filter.cpp
+++ benchmarks/fraud_detection/windflow/fraud_wf_filter/fraud_wf_filter.cpp
@@ -6,26 +6,21 @@
 #include <sstream>
 #include <iostream>
 
-using namespace wf;
-using namespace ff;
-using namespace std;
-
+//using namespace wf;
+//using namespace ff;
+//using namespace std;
 
 // Source_Functor class
 class Source_Functor{
 public:
-    // Constructor
     Source_Functor() {}
-
-    // operator() method
-    void operator()(Source_Shipper<spb::Item> &shipper){
+    void operator()(wf::Source_Shipper<spb::Item> &shipper){
         while (1){ // generation loop
 			spb::Item item;
 			if(!spb::Source::op(item)) break;
-            shipper.push(std::move(item)); // send the next tuple
+            shipper.push(std::move(item));
         }
     }
-    // Destructor
     ~Source_Functor(){}
 };
 
@@ -34,61 +29,54 @@ class Predictor_Functor{
 private: 
 	Markov_Model_Predictor predictor; // predictor state
 public:
-    // Constructor
     Predictor_Functor(){}
-
-    // operator() method
     bool operator()(spb::Item &item){
         spb::Predictor::op(item, predictor);
 		if(item) return true;
 		return false;
     }
-    // Destructor
     ~Predictor_Functor(){}
 };
 
 // Sink_Functor class
 class Sink_Functor{
 public:
-    // Constructor
     Sink_Functor(){}
-
-    // operator() method
     void operator()(optional<spb::Item> &item){
 		spb::Sink::op(*item);
     }
-	// Destructor
 	~Sink_Functor(){}
 };
 
 int main (int argc, char* argv[]){
 	spb::init_bench(argc, argv); // Initializations
 	
-	const string topology_name = "FraudDetection";
+	const std::string topology_name = "FraudDetection";
 
-    PipeGraph topology(topology_name, Execution_Mode_t::DEFAULT, Time_Policy_t::INGRESS_TIME);
+    wf::PipeGraph topology(topology_name, wf::Execution_Mode_t::DEFAULT, wf::Time_Policy_t::INGRESS_TIME);
 
 	Source_Functor source_functor;
-	Source source = Source_Builder(source_functor)
+	wf::Source source = wf::Source_Builder(source_functor)
 						.withParallelism(1)
 						.withName("Source")
 						.withOutputBatchSize(1)
 						.build();
 	Predictor_Functor predictor_functor;
-	Filter predictor = Filter_Builder(predictor_functor)
+	wf::Filter predictor = wf::Filter_Builder(predictor_functor)
 							.withParallelism(spb::nthreads)
 							.withName("Predictor")
 							.withKeyBy([](const spb::Item &item) -> size_t { return item.key; })
 							//.withOutputBatchSize(1)
 							.build();
 	Sink_Functor sink_functor;
-	Sink sink = Sink_Builder(sink_functor)
+	wf::Sink sink = wf::Sink_Builder(sink_functor)
 					.withParallelism(1)
 					.withName("Sink")
 					.build();
-	/// create the application
-	MultiPipe &mp = topology.add_source(source);
-	cout << "Chaining is disabled" << endl;
+
+	// create the application
+	wf::MultiPipe &mp = topology.add_source(source);
+	std::cout << " Chaining is disabled" << std::endl;
 	mp.add(predictor);
 	mp.add_sink(sink);
 
