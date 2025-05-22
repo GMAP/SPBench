18e52385d1d75546025582f0497a7f1c02540abc
Adriano Marques Garcia <adriano1mg@gmail.com>
2025-05-22 20:25:21 +0200

    Update setup_upl.sh

--- libs/upl/setup_upl.sh
+++ libs/upl/setup_upl.sh
@@ -3,7 +3,7 @@
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 cd $THIS_DIR
 
-wget -c --read-timeout=5 --tries=10 https://gmap.pucrs.br/public_data/spbench/libs/upl/upl.tar.xz
+wget -c --read-timeout=5 --tries=10 https://gmap.pucrs.br/public_data/spbench/libs/upl/upl.tar.gz
 
-tar -xf upl.tar.xz
-rm upl.tar.xz
+tar -xf upl.tar.gz
+rm upl.tar.gz
